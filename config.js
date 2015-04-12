6bbb8d9994bbd77624790ebc87cf061646fa0946
toinnem <anthony.meirlaen@gmail.com>
2015-04-12 13:44:48 +0200

    use other twitter oauth client

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 module.exports = {
     twitter: {
-        consumer_key: '',
-        consumer_secret: '',
-        access_token_key: '',
-        access_token_secret: ''
+        consumer_key: 'JWxV8lEkXEVr41mNRncM76Dg4',
+        consumer_secret: 'DgMVkQPiTxYo7up3dABgcKlpLbKNjGw23qmra4srJuIazf5X5h',
+        // Server side tokens
+        access_token_key: '2714561413-kGoMDIzlOg0cGFPleA9WEpWOX3i0ywZYWWETdEU',
+        access_token_secret: 'tu7OTESMaoflnXikdrUEIDLJlsIg6GLgdN0ufRzda2EFa'
     }
 };
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express'),
     util = require('util'),
-    Twitter = require('twitter'),
     config = require('./config');
 
 var app = express();
@@ -15,14 +14,39 @@ app.set('view options', { layout: false }); // NOTE: express3 remove
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-    var client = new Twitter(config.twitter);
+/**
+ * TWITTER oauth authentication
+ */
+var twitterAPI = require('node-twitter-api');
+var twitter = new twitterAPI({
+    consumerKey: config.consumer_key,
+    consumerSecret: config.consumer_secret,
+    callback: 'https://topkerels.herokuapp.com/services/twitter/oauth'
+});
+var fakeTwitterDb = {};
 
-    var params = {screen_name: 'nodejs'};
-    client.get('statuses/user_timeline', params, function(error, tweets, response){
-        if (!error) {
-            console.log(tweets);
-            res.render('main');
+
+app.get('/', function (req, res) {
+    res.render('main');
+});
+
+app.get('/authenticate', function (req, res) {
+    twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+        if (error) {
+            console.log("Error getting OAuth request token : " + error);
+        } else {
+            fakeTwitterDb[requestToken] = requestTokenSecret;
+            res.redirect(twitter.getAuthUrl(requestToken));
+        }
+    });
+});
+
+app.get('/oauth/twitter/callback', function (req, res) {
+    twitter.getAccessToken(req.oauth_token, fakeTwitterDb[req.oauth_token], req.oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+        if (error) {
+            console.log(error);
+        } else {
+            req.cookie('credentials', '1', {accessToken: accessToken, accessTokenSecret: accessTokenSecret});
         }
     });
 });
